9b4cad18b4c0ec2c361c3462b89c52d558a8bd73
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-04-13 10:53:28 -0400

    Added questions and answer parameters

--- Program.cs
+++ Program.cs
@@ -2,20 +2,25 @@
 
 namespace EnthusiasticMoose
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine();
-            MooseSays("H I, I'M  E N T H U S I A S T I C !");
-    MooseSays("I really am enthusiastic");
-        }
+      Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+      Console.WriteLine("--------------------------------------------");
+      Console.WriteLine();
+      // Let the moose speak!
+      MooseSays("H I, I'M  E N T H U S I A S T I C !");
+      MooseSays("I really am enthusiastic");
 
-        static void MooseSays(string message)
-        {
-            Console.WriteLine($@"
+      // As a question
+      bool isTrue = MooseAsks("Is Canada real?");
+      Console.WriteLine(isTrue);
+    }
+
+    static void MooseSays(string message)
+    {
+      Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -43,6 +48,27 @@ namespace EnthusiasticMoose
                       \ \'._  ` '_.'
                        `^^` `^^^`
             ");
-        }
     }
+
+    static bool MooseAsks(string question)
+    {
+      Console.Write($"{question} (Y/N): ");
+      string answer = Console.ReadLine().ToLower();
+
+      while (answer != "y" && answer != "n")
+      {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+      }
+
+      if (answer == "y")
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
 }
